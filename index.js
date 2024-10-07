87b196c708200e7f63dac9b4c783d6851cd17801
MASUMHD <mrmasum2k4@gmail.com>
2024-10-07 22:08:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById('employeeForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const employeeData = {
+      profilePhoto: document.getElementById('profilePhoto').files[0] ? document.getElementById('profilePhoto').files[0].name : 'No file selected',
+      firstName: document.getElementById('firstName').value,
+      lastName: document.getElementById('lastName').value,
+      employeeID: document.getElementById('employeeID').value,
+      email: document.getElementById('email').value,
+      phoneNumber: document.getElementById('phoneNumber').value,
+      department: document.getElementById('department').value,
+      designation: document.getElementById('designation').value,
+      gender: document.getElementById('gender').value,
+      dob: document.getElementById('dob').value,
+      aboutMe: document.getElementById('aboutMe').value,
+      employeeStatus: document.getElementById('employeeStatus').value
+    };
+
+    console.log(employeeData);
+});
